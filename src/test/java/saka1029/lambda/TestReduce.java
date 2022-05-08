f0df5121399da1c8753f84506a763259caeac834
saka1029 <saka1029@gmail.com>
2022-05-09 06:55:36 +0900

    Update TestReduce.java

--- src/test/java/saka1029/lambda/TestReduce.java
+++ src/test/java/saka1029/lambda/TestReduce.java
@@ -7,14 +7,44 @@ import static saka1029.lambda.LambdaCalculus.*;
 
 public class TestReduce {
 
+    static BoundVariable local(String name) {
+        return BoundVariable.of(name);
+    }
+
+    static Lambda lambda(BoundVariable v, Expression e) {
+        return Lambda.of(v, e);
+    }
+
+    static FreeVariable free(String name) {
+        return FreeVariable.of(name);
+    }
+
+    static Application apply(Expression h, Expression t) {
+        return Application.of(h, t);
+    }
+
     @Test
     public void testFreeVariable() {
         Binder<FreeVariable, Expression> frees = new Binder<>();
-        Lambda lambda = (Lambda)parse("λx.x");
-        try (Unbind u = frees.bind(FreeVariable.of("a"), lambda)) {
-            assertEquals(lambda, reduce(parse("a"), frees));
+        BoundVariable x = local("x");
+        Lambda lambda = lambda(x, x);
+        FreeVariable a = free("a");
+        try (Unbind u = frees.bind(a, lambda)) {
+            assertEquals(lambda, reduce(a, frees));
         }
-        assertNull(frees.get(FreeVariable.of("a")));
+        assertNull(frees.get(a));
+    }
+
+    @Test
+    public void testApplication() {
+        // λx.x
+        BoundVariable x = local("x");
+        Lambda xx = lambda(x, x);
+        // λy.y y
+        BoundVariable y = local("y");
+        Lambda yyy = lambda(y, apply(y, y));
+        // λy.y y = reduce(λx.x (λy.y y))
+        assertEquals(yyy, reduce(apply(xx, yyy)));
     }
     
 }
