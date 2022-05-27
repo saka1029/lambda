915ea3f4bbd27067b5df80872eee55d43eafb31e
saka1029 <saka1029@gmail.com>
2022-05-27 21:52:50 +0900

    Update Lambda.java

--- src/main/java/saka1029/lambda/Lambda.java
+++ src/main/java/saka1029/lambda/Lambda.java
@@ -20,11 +20,11 @@ public class Lambda extends Expression {
      * η変換可能であれば、その結果を返します。
      * 不可能な場合はnullを返します。
      * 
-     * η変換は2つの関数が全ての引数に対して常に同じ値を返すようなとき、互いに同値であるとみなすという概念である。
+     * η変換は2つの関数が全ての引数に対して常に同じ値を返すようなとき、互いに同値であるとみなすという概念です。
      * <code>
      * λV.E V →η E
      * </code>
-     * ただし、EにVが自由出現しないときに限る。
+     * ただし、EにVが自由出現しないときに限ります。
      */
     public Expression etaConversion() {
     	if (refCount == 1 && body instanceof Application a && a.tail.equals(variable))
